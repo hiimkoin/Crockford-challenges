e6d8723f6bb4a7de840c2efd4cf20216635d6602
Robert Smith <rbrtsmith84@googlemail.com>
2015-09-26 15:34:34 +0200

    Create 2-ES5.js

---
+++ 2-ES5.js
@@ -0,0 +1,13 @@
+/*
+ * Write two binary functions, 'add' and 'mul', that take two
+ * numbers and return their sum and product.
+**/
+
+
+function add(x,y) {
+  return x + y;
+}
+
+function mul(x,y) {
+  return x * 6;
+}
