8894c85944ea24b77a96d4b511d0ebb88e7ca849
Robert Smith <rbrtsmith84@googlemail.com>
2015-09-27 00:30:38 +0200

    Create 10-ES5.js

---
+++ 10-ES5.js
@@ -0,0 +1,9 @@
+/*
+ * Write a function 'twice' that takes a binary function and returns a unary function, that passes it's argument to the binary function twice.
+**/
+
+function twice(fn) {
+  return function(x) {
+    return fn(x,x);
+  };
+}
