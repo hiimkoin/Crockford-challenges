1cfe23dca14f4552fc8ae50299d065748be89d98
Robert Smith <rbrtsmith84@googlemail.com>
2015-09-26 16:32:36 +0200

    Create 5-ES5.js

---
+++ 5-ES5.js
@@ -0,0 +1,12 @@
+/*
+ * Write a function that takes a binary function, and makes it callable
+ * with two invocations.
+**/
+
+function applyf(fn) {
+  return function(x) {
+    return function(y) {
+      return fn(x,y);
+    }
+  }
+}
