fe6452709eba1b1689ad74e2c4ab69ac87748da7
Robert Smith <rbrtsmith84@googlemail.com>
2015-09-26 15:42:00 +0200

    Create 3-ES5.js

---
+++ 3-ES5.js
@@ -0,0 +1,10 @@
+/*
+ * Write a function that takes an argument and returns a 
+ * function that returns that argument
+**/
+
+function identityf(arg) {
+  return function() {
+    return arg;
+  }
+}
